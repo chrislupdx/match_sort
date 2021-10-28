d97d7ce99e81415baf686dd4e7a41cc63a9b5952
chrislupdx <christopherlu.pdx@gmail.com>
2021-10-27 17:55:39 -0700

    building out text generation

--- match.py
+++ match.py
@@ -1,3 +1,4 @@
+from random import Random
 #this is a python 3 implementation of a brute-force matching sort algorithm
 def bfmatch(text, pattern):
     textlen = len(text)
@@ -5,29 +6,48 @@ def bfmatch(text, pattern):
     deltlen = textlen - patlen
     i = 0 # pattern iterator
     success = False
-
     while(i < deltlen):  
         j = 0
-        #print the base text 
         for a in text:
             print(a, end="")
         print("")
-
-        while(j < patlen and (pattern[j] == text[i + j])):  
-            #produce spaces + display pattern[j]
+        while(j < patlen and (pattern[j] == text[i + j])):
             x = 0
             while (x < i + j):
                 print(" ", end="")
                 x += 1
             print(pattern[j])
             j += 1
-
         if(j == patlen): #if we reached the end of the pattern
             success = True
-            return #might be redundant
-        #print("i is", i)
+            return
         i += 1
     print(success) 
-text = "There are woundrous things. There are magical things. There are dangerous things. We cake"
-pattern  = "There are dangerous things."
-bfmatch(text, pattern)
+
+def interface():
+    #print("Match Sort Algorithm, pick a size of your base string")
+    #text_size = int(input())
+    #
+    #rand_text = [text_size]
+    #for char in rand_text:
+    #    #temp = pick 1 / 128
+    #    temp_char = chr(temp)
+    #    rand_text.append(temp_char)
+
+    #generate pattern of  the same type as 
+    #generate an interface funciton
+    #default_text = "There are woundrous things. There are magical things. There are dangerous things. We get what we deserve."
+    #default_pattern  = "There are dangerous things."
+    text = ""
+    pattern = ""
+
+    bfmatch(text, pattern)
+
+    #how to design wall clock experimetns
+        #upon execution start time
+        #upon execution end, 
+        #calculate the difference
+    #wall_clock_data = []
+    #display the wall clock experiment data
+
+interface()
